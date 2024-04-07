be5a1bc0db5038b11e702990976c66b0e6960011
basile atsougan <atsougan870@gmail.com>
2024-04-07 10:34:28 +0200

    updates

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import openai
+from django.contrib import auth
+
+
 # Create your views here.
 
+#openai.api_key = "sk-"
+messages = [{"role": "system", "content": "You are a chatGPT"}]
 
+def ask_openai(user_input):
+    messages.append({"role": "user", "content": user_input})
 
-
-openai.api_key = ""
-messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
-
-def ask_openai(messages):
-    messages.append({"role": "user", "content": messages})
-    response = openai.Completion.create(
+    response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
-        messages = messages,
-        
+        messages = messages
     )
-    ChatGPT_reply = response["choices"][0]["message"]["content"]
-    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    answer = response.choices[0].message.content
+    return answer
+
+
     #return ChatGPT_reply
-    print(ChatGPT_reply) 
-    
+    #print(response) 
+    # strip to remove all formatting of text and just return the txt
 
 
 
@@ -31,3 +33,25 @@ def chatbot(request):
 
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+
+
+
+
+
+
+
+
+
+
+
+
